5c5761b3e1c8e09b25d991000f910dc354710394
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-24 12:36:54 +0900

    style: load clarity-first UI overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './integration.css';
+import './ux.css';
 import './accessibility.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
